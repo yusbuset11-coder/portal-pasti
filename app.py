0c1e89e33027c3a95711dc50f0bd4f44b3a029dc
yusbuset11-coder <yusbuset11@gmail.com>
2026-08-16 11:47:56 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1102,10 +1102,10 @@ elif pilih_app == "2. SIPENSIS (Sistem Informasi Presensi Siswa)":
     df_template["I"] = False
     df_template["A"] = False
 df_template = df_template[["Tanggal", "Sekolah", "Nama_Guru", "Mata_Pelajaran", "Kelas", "ID_Siswa", "Nama_Siswa", "Status_Kehadiran", "S", "I", "A"]]
-            output = io.BytesIO()
-            with pd.ExcelWriter(output, engine='openpyxl') as writer:
-                df_template.to_excel(writer, index=False, sheet_name='Template_Absensi')
-            excel_data = output.getvalue()
+  output = io.BytesIO()
+  with pd.ExcelWriter(output, engine='openpyxl') as writer:
+df_template.to_excel(writer, index=False, sheet_name='Template_Absensi')
+  excel_data = output.getvalue()
 
             st.download_button(
                 label="📥 Download Template Absensi Kelas",
